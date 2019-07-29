0f0687466377d57f77e5ac40aeff4c01ec435ec4
Dani <daniel.mengistu@hotmail.com>
2019-07-29 22:58:06 +0300

    error hondleing set up

--- src/main/java/com/danimeko/blog/controller/BlogController.java
+++ src/main/java/com/danimeko/blog/controller/BlogController.java
@@ -1,10 +1,14 @@
 package com.danimeko.blog.controller;
 
+import com.danimeko.blog.exception.ResourceNotFoundException;
 import com.danimeko.blog.model.Blog;
 import com.danimeko.blog.service.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -16,15 +20,18 @@ public class BlogController {
     @Autowired
     BlogRepository blogRepository;
 
-    @RequestMapping(value="blog" ,method = RequestMethod.GET)
+    @GetMapping("/blog")
     public List<Blog> index(){
         return blogRepository.findAll();
     }
 
-   @GetMapping("/blog/{id}")
-    public Optional<Blog> show(@PathVariable String id){
+    @GetMapping("/blog/{id}")
+    public ResponseEntity<Blog> getBlogById(@PathVariable String id)
+            throws ResourceNotFoundException {
         int blogId = Integer.parseInt(id);
-        return blogRepository.findById(blogId);
+        Blog blog = blogRepository.findById(blogId)
+                .orElseThrow(() -> new ResourceNotFoundException("There is no blog with this id :: " + id));
+        return ResponseEntity.ok().body(blog);
     }
 
     @PostMapping("/blog/search")
@@ -34,27 +41,34 @@ public class BlogController {
     }
 
     @PostMapping("/blog")
-    public Blog create(@RequestBody Map<String, String> body){
-        String title = body.get("title");
-        String content = body.get("content");
-        return blogRepository.save(new Blog(title, content));
+    public Blog create(@Valid @RequestBody Blog newBlog){
+        return blogRepository.save(newBlog);
     }
 
     @PutMapping("/blog/{id}")
-    public Blog update(@PathVariable String id, @RequestBody Map<String, String> body){
+    public ResponseEntity<Blog> update(@PathVariable String id, @RequestBody Blog updateBlog)
+            throws ResourceNotFoundException{
         int blogId = Integer.parseInt(id);
         // getting blog
-        Blog blog = blogRepository.findById(blogId).orElse(null);
-        blog.setTitle(body.get("title"));
-        blog.setContent(body.get("content"));
-        return blogRepository.save(blog);
+        Blog blog = blogRepository.findById(blogId)
+                .orElseThrow(()-> new ResourceNotFoundException("Blog not found with this id ::" + id ));
+        blog.setTitle( updateBlog.getTitle());
+        blog.setContent( updateBlog.getContent());
+        final Blog updatedBlog = blogRepository.save(blog);
+        return ResponseEntity.ok(updatedBlog);
     }
 
     @DeleteMapping("blog/{id}")
-    public boolean delete(@PathVariable String id){
+    public Map<String , Boolean> delete(@PathVariable String id)
+            throws ResourceNotFoundException{
+
         int blogId = Integer.parseInt(id);
+        Blog blog=blogRepository.findById(blogId)
+                .orElseThrow(() -> new ResourceNotFoundException("Blog not found for this id :: " + id));
         blogRepository.deleteById(blogId);
-        return true;
+        Map<String ,Boolean> responce = new HashMap<>();
+        responce.put("deleted",Boolean.TRUE);
+        return responce;
     }
 
 }
